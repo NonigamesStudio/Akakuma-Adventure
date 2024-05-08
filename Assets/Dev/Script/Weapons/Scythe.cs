3984875c5eccc8318563a0da7837a1395c90aae0
Fede <fede@MBP-de-Fede.cust.communityfibre.co.uk>
2024-05-08 19:49:36 +0100

    sadf

--- Assets/Dev/Script/Weapons/Scythe.cs
+++ Assets/Dev/Script/Weapons/Scythe.cs
@@ -14,7 +14,6 @@ public class Scythe : MonoBehaviour, IWeapon
     [SerializeField] float attackDuration;
     [SerializeField] float tickTimeDmg;
     [SerializeField] float coolDown;
-    [SerializeField] GameObject PacticleSlash;
     [SerializeField] LayerMask mask;
     [SerializeField] bool isEnemy;
 
@@ -27,8 +26,7 @@ public class Scythe : MonoBehaviour, IWeapon
         {
             if (!isEnemy) AnimController_Player.ins.PlayAnim(AnimNamesPlayer.AttackScythe);
             StartCoroutine(AttackAction(bonusDmg));
-            StartCoroutine(SkillAnim());
-            if (!isEnemy) player.OnWeaponAttack?.Invoke();
+            player.OnWeaponAttack?.Invoke();
             isOnCoolDownNormalAttack = true;
             LeanTween.delayedCall(coolDown, () => { isOnCoolDownNormalAttack = false; });
             
@@ -40,9 +38,10 @@ public class Scythe : MonoBehaviour, IWeapon
 
     IEnumerator AttackAction(float bonusdmg)
     {
-        float time = 0;
+        yield return new WaitForSeconds(0.3f);
+       
         
-        while (attackDuration > time)
+        for (int i = 0; i < colliderAttack.Count; i++)
         {
             yield return new WaitForSeconds(0.2f);
             Collider[] results = Physics.OverlapBox(transform.position, colliderAttack[i].size, Quaternion.identity, mask);
@@ -59,71 +58,8 @@ public class Scythe : MonoBehaviour, IWeapon
 
              
         }
-        PacticleSlash.SetActive(false); ;
     }
 
-    public void Skill()
-    {
-        if (isOnCoolDownSkill) return;
-        isOnCoolDownSkill = true;
-        StartCoroutine(SkillAnim());
-        StartCoroutine(SkillAction());
-    }
-
-    void SkillCoolDown()
-    {
-        LeanTween.delayedCall(coolDownSkill, () => { isOnCoolDownSkill = false; });
-    }
-
-    IEnumerator SkillAction()
-    {
-        player.onSkill = true;
-        float time = 0;
-        player.OnWeaponSkill?.Invoke();
-        while (skillDuration > time)
-        {
-            Collider[] results = Physics.OverlapSphere(transform.position, colliderSkill.radius, mask);
-
-            foreach (Collider objectColli in results)
-            {
-                if (objectColli.TryGetComponent<Health>(out Health health))
-                {
-                    if (gameObject.layer != objectColli.gameObject.layer) health.TakeDamage(damage, transform.root);
-                }
-            }
-
-            time += tickTimeDmg;
-
-            yield return new WaitForSeconds(tickTimeDmg);
-        }
-        player.onSkill = false;
-        pivotToSkill.localEulerAngles = Vector3.zero;
-        SkillCoolDown();
-    }
-
-    IEnumerator SkillAnim()
-    {
-        player.GetStuned(1f);
-        yield return new WaitForSeconds(0.5f);
-        PacticleSlash.SetActive(true);
-        yield return new WaitForSeconds(1f);
-        PacticleSlash.SetActive(false);
-
-        //skillParticle.SetActive(true);
-
-        //float time = skillDuration;
-        //float ytemp = 0;
-        //while (time>0)
-        //{
-        //    pivotToSkill.localEulerAngles =new Vector3(0, ytemp*10, 0);
-        //    ytemp += Time.deltaTime * speedRotSkill;
-        //    time -= Time.deltaTime;
-        //    yield return null;
-        //}
-        //pivotToSkill.localEulerAngles = Vector3.zero;
-
-        //skillParticle.SetActive(false);
-    }
 
     public void TurnOnOffWeapon(bool turnOnOff)
     {
